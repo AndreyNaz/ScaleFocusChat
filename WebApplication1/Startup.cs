1e919702970e5e3dedad30121edee5c38217cc3e
Andrey Nazarov <andreynaz@abv.bg>
2019-01-14 02:27:24 +0200

    Created and configured web api back end Started work on react front end Lots and lots and lots and lots of trial and error so far

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using ScaleFocusChat.Data;
-using ScaleFocusChat.Hubs;
 
-[assembly: ApiController]
-namespace ScaleFocusChat
+namespace WebApplication1
 {
     public class Startup
     {
@@ -32,16 +22,11 @@ namespace ScaleFocusChat
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<ScaleFocusChatDbContext>(options
-                => options.UseSqlServer(Configuration.GetConnectionString("ScaleFocusChatConnection")));
-
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp2/build";
+                configuration.RootPath = "ClientApp/build";
             });
-
-            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,29 +38,30 @@ namespace ScaleFocusChat
             }
             else
             {
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
-
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp2";
+                spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
                 {
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
-            });
-
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<ChatHub>("/chatHub");
             });
         }
     }
